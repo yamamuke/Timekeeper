c66f0971c08f43d14a9ac94165ac01ccb439c466
EC2 Default User <ec2-user@ip-172-31-26-229.ap-northeast-1.compute.internal>
2022-08-13 14:21:18 +0000

    login screen

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "timelog_app_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.default_url_options = { host: "localhost", port: ENV.fetch("PORT", 3000) }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
